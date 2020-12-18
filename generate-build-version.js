bee540c285a44d797656308ee712e7b31e6615db
Nicholas Paulik <nicholas.paulik@schibsted.no>
2020-12-18 03:57:08 +0100

    Save

--- generate-build-version.js
+++ generate-build-version.js
@@ -1,18 +1,10 @@
 const fs = require('fs');
-const packageJson = require('./package.json');
+const { version } = require('./package.json');
 
-const appVersion = packageJson.version;
-
-const jsonData = {
-  version: appVersion,
-};
-
-const jsonContent = JSON.stringify(jsonData);
-
-fs.writeFile('./public/meta.json', jsonContent, 'utf8', (error) => {
+fs.writeFile('./public/meta.json', JSON.stringify({ version }), 'utf8', (error) => {
   if (error) {
-    console.log('An error occured while writing JSON Object to meta.json');
-    console.log(error);
+    console.log('An error occured while writing JSON Object to meta.json.');
+    throw error;
   }
-  console.log('meta.json file has been saved with latest version number');
+  console.log('meta.json file has been saved with latest version number.');
 });
--- src/index.tsx
+++ src/index.tsx
@@ -52,7 +52,7 @@ firebase.auth().onAuthStateChanged((user) => {
     const ui = new firebaseui.auth.AuthUI(firebase.auth());
     ui.start('#firebaseui-auth-container', {
       signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-      // Other config options...
+      signInSuccessUrl: '/',
     });
   }
 });
