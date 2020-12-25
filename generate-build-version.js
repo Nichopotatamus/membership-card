bfdb2188871bb1ed134f1f7bac033d8a68f41009
Nicholas Paulik <nicholas.paulik@schibsted.no>
2020-12-25 22:50:48 +0100

    Versioning fixes

--- generate-build-version.js
+++ generate-build-version.js
@@ -3,7 +3,7 @@ const { version } = require('./package.json');
 
 fs.writeFile('./public/meta.json', JSON.stringify({ version }), 'utf8', (error) => {
   if (error) {
-    console.log('An error occured while writing JSON Object to meta.json.');
+    console.log('An error occurred while writing JSON Object to meta.json.');
     throw error;
   }
   console.log('meta.json file has been saved with latest version number.');
